a1958f8d85c3d33247bd63de83f5a5be3bc7ec49
Nayeon-623 <qoskdus1@naver.com>
2023-05-14 20:18:47 +0900

    0514_영수증_게시판 작업
    영수증 양식 추가
    게시판 추가
    Purchase_Order_M에 양식 버튼을 각각의 양식 파일에 연결

---
+++ views/js/Purchase_Order_M_button.js
@@ -0,0 +1,35 @@
+function F_Trading_statement() {
+  PO1.style.display = "block";
+  PO2.style.display = "none";
+  PO3.style.display = "none";
+  PO4.style.display = "none";
+  PO5.style.display = "none";
+}
+function F_Bill() {
+  PO1.style.display = "none";
+  PO2.style.display = "block";
+  PO3.style.display = "none";
+  PO4.style.display = "none";
+  PO5.style.display = "none";
+}
+function F_Purchase_Order() {
+  PO1.style.display = "none";
+  PO2.style.display = "none";
+  PO3.style.display = "block";
+  PO4.style.display = "none";
+  PO5.style.display = "none";
+}
+function F_Tax_Bill() {
+  PO1.style.display = "none";
+  PO2.style.display = "none";
+  PO3.style.display = "none";
+  PO4.style.display = "block";
+  PO5.style.display = "none";
+}
+function F_Board() {
+  PO1.style.display = "none";
+  PO2.style.display = "none";
+  PO3.style.display = "none";
+  PO4.style.display = "none";
+  PO5.style.display = "block";
+}

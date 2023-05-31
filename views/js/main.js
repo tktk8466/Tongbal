adfa6e1dc296c0c44c1934586069637477ff9d93
tktk8466 <tktk8466@gmail.com>
2023-05-31 22:50:09 +0900

    23-05-31
    WebSocket_Module.js / ChatMsg_query.js / PO_query.js
    Purchase_Order_Received.ejs
    Purchase_Order_Send.ejs
    main.js / main.css
     - 채팅창에 [회사명] 유저명 표기를 위해 DB 컬럼 수정 및 함수 수정
    Purchase_Order_Received_form.ejs
    Purchase_Order_Send_form.ejs
     - 발주서 수정 창 연결 -> URL에 type과 key 삽입
     - 발주서 내용 (Content) 작업
    
    Purchase_Order_edit.ejs
     - 발주서 수정 창 작업
    
    main_login.ejs
     - 비정상 접근 차단 작업
     - 웹소켓 접속 삭제
    
    MyPage.ejs
     - 비정상 접근 처리

--- views/js/main.js
+++ views/js/main.js
@@ -1,15 +1,3 @@
-//input 적은거 새로 읽음
-function SetInput() {
-  const Company_Name = document.getElementById("Company_Name").value;
-  const CEO = document.getElementById("CEO").value;
-  const Company_Address = document.getElementById("Company_Address").value;
-  const Business_Address = document.getElementById("Business_Address").value;
-  const Business = document.getElementById("Business").value;
-  const Business_Kind = document.getElementById("Business_Kind").value;
-  const Company_Number = document.getElementById("Company_Number").value;
-  const Fax = document.getElementById("Fax").value;
-}
-
 function reload() {
   (location || window.location || document.location).reload();
 }
@@ -27,8 +15,8 @@ function toggleDiv(div) {
 //테이블 클릭시 div 전환함수
 function toggletoggle() {
   //실제 창 전환 함수
-  toggleDiv(myDIV);
-  toggleDiv(myDIV2);
+  toggleDiv(PO_CHAT_DIV);
+  toggleDiv(PO_CHAT_DIV2);
   toggleDiv(POtable);
 } //function
 
@@ -40,68 +28,56 @@ function removeAllchild(div) {
 }
 
 //<!--송신자 메시지-->
-function newSendDiv(id, text) {
+function newSendDiv(id, text, username, compname) {
   // 1. <div> element 만들기
-  const newDiv = document.createElement("div");
-  const newDiv2 = document.createElement("div");
-  const newDiv3 = document.createElement("div");
-  const I1 = document.createElement("i");
+  const chat = document.createElement("div");
+  const icon = document.createElement("div");
+  const textbox = document.createElement("div");
+  const userinfo = document.createElement("div");
 
   // 2. <div>에 들어갈 text node 만들기
-  newDiv.className = "chat ch2";
-
-  newDiv2.className = "icon";
-  newDiv3.className = "textbox";
-  I1.className = "fa-solid fa-user";
-
-  const newText = text;
+  chat.className = "chat mytext";
+  icon.className = "icon";
+  textbox.className = "textbox";
+  userinfo.style = "width:100%; text-align:right; padding-right:25px";
+  userinfo.append("[" + compname + "] " + username);
 
   // 3. <div>에 text node 붙이기
-  newDiv3.append(newText);
-  newDiv2.append(I1);
+  textbox.append(text);
 
   // 4. <body>에 1에서 만든 <div> element 붙이기
-  newDiv.append(newDiv2);
-  newDiv.append(newDiv3);
-  id.append(newDiv);
-
-  //아래 구조로 만들어서 채팅방에 쏘는 것임.
-  //<div class = "chat ch2">
-  //	<div class = "icon">
-  //		<i class = "textbox">txt_on에 친 내용이 여기에 들어감</i>
-  //
-  //	</div>
-  //</div>
+  chat.append(textbox);
+  id.append(userinfo);
+  id.append(chat);
 }
 
 //<!--수신자 메시지 창-->
-function newReceiveDiv(id, text) {
+function newReceiveDiv(id, text, username, compname) {
   // 1. <div> element 만들기
-  const newDiv = document.createElement("div");
-  const newDiv2 = document.createElement("div");
-  const newDiv3 = document.createElement("div");
-  const I1 = document.createElement("i");
+  const chat = document.createElement("div");
+  const icon = document.createElement("div");
+  const textbox = document.createElement("div");
+  const userinfo = document.createElement("div");
 
   // 2. <div>에 들어갈 text node 만들기
-  newDiv.className = "chat ch1";
-  newDiv2.className = "icon";
-  newDiv3.className = "textbox";
-  I1.className = "fa-solid fa-user";
-
-  const newText = text;
+  chat.className = "chat optext";
+  icon.className = "icon";
+  textbox.className = "textbox";
+  userinfo.style = "width:100%; text-align: left; padding-left:25px";
+  userinfo.append("[" + compname + "] " + username);
 
   // 3. <div>에 text node 붙이기
-  newDiv3.append(text);
-  newDiv2.append(I1);
+  textbox.append(text);
+
   // 4. <body>에 1에서 만든 <div> element 붙이기
-  newDiv.append(newDiv2);
-  newDiv.append(newDiv3);
-  id.append(newDiv);
+  chat.append(textbox);
+  id.append(userinfo);
+  id.append(chat);
 }
 
 // 메세지 발신
 
-function sendMessage(event, key, UserID, UserName, chat_room, div_id) {
+function sendMessage(event, key, UserID, UserName, CompName, chat_room, div_id) {
   const textarea = document.getElementById("txt_on_" + key);
   if (event != undefined && textarea.value != undefined) {
     let key = event.key || event.keyCode;
@@ -111,10 +87,11 @@ function sendMessage(event, key, UserID, UserName, chat_room, div_id) {
         msg: textarea.value,
         user_id: UserID,
         user_name: UserName,
+        comp_name: CompName,
         chat_room_id: chat_room,
       });
       ws.send(fullMessage);
-      newSendDiv(div_id, textarea.value);
+      newSendDiv(div_id, textarea.value, UserName, CompName);
       textarea.value = "";
     }
   } else if (event == undefined && textarea.value != undefined) {
@@ -124,10 +101,11 @@ function sendMessage(event, key, UserID, UserName, chat_room, div_id) {
       msg: textarea.value,
       user_id: UserID,
       user_name: UserName,
+      comp_name: CompName,
       chat_room_id: chat_room,
     });
     ws.send(fullMessage);
-    newSendDiv(div_id, textarea.value);
+    newSendDiv(div_id, textarea.value, UserName, CompName);
     textarea.value = "";
   } else {
     console.log("Main.js :: sendMessage() : message is undefined");

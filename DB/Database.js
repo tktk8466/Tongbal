f1c29d6bfcd9c8d9b2bea7abf109b7a77dcb6201
재승 이 <tktk8466@gmail.com>
2023-12-03 23:05:39 +0900

    DB 연결 수정
    DB 연결 정보를 json 파일에서 불러오도록 수정

--- DB/Database.js
+++ DB/Database.js
@@ -1,21 +1,14 @@
 const mysql = require("mysql2/promise"); // mysql 모듈 로드
 const session = require("express-session");
 const MySQLStore = require("express-mysql-session")(session);
-
 const time = require("../views/js/time.js");
+const fs = require("fs");
+
+// MySQL 로그인 정보 파일 읽기
+const dbConfig = JSON.parse(fs.readFileSync("DB_data.json", "utf8"));
+console.log(dbConfig);
 
 // MYSQL CONNECTION
-const dbConfig = {
-  // mysql 접속 설정
-  host: "",
-  port: "",
-  user: "",
-  password: "",
-  database: "",
-  connectionLimit: 100,
-  dateStrings: "date",
-};
-
 const DB = mysql.createPool(dbConfig);
 
 var sessionStore = new MySQLStore(dbConfig);
@@ -23,11 +16,14 @@ var sessionStore = new MySQLStore(dbConfig);
 module.exports = {
   conn: async () => {
     return new Promise((resolve, reject) => {
+      str = time.timeString() + "database :: DB 접속 부여중...";
       try {
         resolve(DB.getConnection());
+        console.log(str + "DONE");
       } catch (err) {
         reject();
-        console.log(time.timeString() + "database :: DB 접속 부여중...FAILED");
+        console.log(str + "FAILED");
+        console.error(err);
       }
     });
   },
